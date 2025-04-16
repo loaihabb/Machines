3ff2e30276e5153aadd60a756595bdeed33c8d78
loaihabb <loaihabb@gmail.com>
2025-04-16 11:03:43 +0300

    some changes

--- app.js
+++ app.js
@@ -167,6 +167,9 @@ function setRoleUI(role) {
       group.style.display = isAdmin ? "flex" : "none";
     });
     document.getElementById("reset-all-btn").style.display = isAdmin ? "inline-block" : "none";
+    document.getElementById("time-unit-selector").style.display = isAdmin ? "inline-block" : "none";
+    document.getElementById("generate-graph-btn").style.display = isAdmin ? "inline-block" : "none";
+
   }
   
 
